674f74c7b3d0addd153f6242f786a3f15d6402e7
Max Sikström <max@pengi.se>
2024-08-24 10:08:14 +0200

    Update degrees per pixel calculation
    For really small FOV:s, the assumption that the hud is spherical works
    well. But that's not true for normal FOV:s
    
    therefore, do the calculation for degrees per pixel at center of screen
    so the horizon line sticks to the environment when at level.
    
    Improves the "stickiness" of the HUD to the environment

--- src/main/java/net/torocraft/flighthud/Dimensions.java
+++ src/main/java/net/torocraft/flighthud/Dimensions.java
@@ -3,6 +3,8 @@ package net.torocraft.flighthud;
 import net.minecraft.client.MinecraftClient;
 import net.torocraft.flighthud.config.HudConfig;
 
+import java.lang.Math;
+
 public class Dimensions {
 
   public float hScreen;
@@ -31,7 +33,31 @@ public class Dimensions {
       wScreen = wScreen * c.scale;
     }
 
-    degreesPerPixel = hScreen / client.options.getFov().getValue();
+    // The HUD is rendered as a plane in front of the player and not at a
+    // spehere. The degreesPerPixel parameter is therefore a simplification,
+    // which is not exact.
+    //
+    // To use this simplification, a given spot on the screen has to be picked
+    // to be be correct for the approximation.
+    //
+    // Most intuitive would then be to have the horizon line correctly stick to
+    // the world when in the center of the screen. So therefore, calculate the
+    // number of degrees per pixel in the _center_ of the screen
+    //
+    // This simplifies rendering a lot, instead of always calculate the correct
+    // transformation between spherical and cartesian coordinates
+
+
+    // Calculate height of virtual screen at distance of 1 unit
+    // Note that FOV is degrees from top to bottom, we need height from center
+    // to top
+
+    // Also note that "degreesPerPixel" is actually pixels per degree...
+    Integer fov_deg = client.options.getFov().getValue();
+    double hud_height = Math.tan(fov_deg * Math.PI / 180.0 / 2.0);
+    double hud_pixel_height = hud_height / (double)(hScreen/2);
+    degreesPerPixel = 1.0f / (float)(Math.atan(hud_pixel_height) * 180.0 / Math.PI);
+
     xMid = wScreen / 2;
     yMid = hScreen / 2;
 
